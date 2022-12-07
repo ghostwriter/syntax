26044680d79c47be008369fa7baa6af860719f8a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-07 16:48:12 -0600

    Create CharacterCodes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/CharacterCodes.php
@@ -0,0 +1,214 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Plex;
+
+final class CharacterCodes
+{
+    public const _0 = 0x30;
+
+    public const _1 = 0x31;
+
+    public const _2 = 0x32;
+
+    public const _3 = 0x33;
+
+    public const _4 = 0x34;
+
+    public const _5 = 0x35;
+
+    public const _6 = 0x36;
+
+    public const _7 = 0x37;
+
+    public const _8 = 0x38;
+
+    public const _9 = 0x39;
+
+    public const _AMPERSAND = 0x26; // &
+
+    public const _ASTERISK = 0x2A; // *
+
+    public const _AT = 0x40; // @
+
+    public const _BACKSLASH = 0x5C; // \
+
+    public const _BACKSPACE = 0x08; // \b
+
+    public const _BACKTICK = 0x60; // `
+
+    public const _BAR = 0x7C; // |
+
+    public const _BYTE_ORDER_MARK = 0xFEFF;
+
+    public const _CARET = 0x5E; // ^
+
+    public const _CLOSE_BRACE = 0x7D; // }
+
+    public const _CLOSE_BRACKET = 0x5D; // ]
+
+    public const _CLOSE_PARENTHESES = 0x29; // )
+
+    public const _COLON = 0x3A; // :
+
+    public const _COMMA = 0x2C; // ;
+
+    public const _DOLLAR = 0x24; // $
+
+    public const _DOT = 0x2E; // .
+
+    public const _DOUBLE_QUOTE = 0x22; // "
+
+    public const _EQUALS = 0x3D; // =
+
+    public const _EXCLAMATION = 0x21; // !
+
+    public const _FORM_FEED = 0x0C; // \f
+
+    public const _GREATER_THAN = 0x3E; // >
+
+    public const _HASH = 0x23; // #
+
+    public const _LESS_THAN = 0x3C; // <
+
+    public const _MINUS = 0x2D; // -
+
+    public const _NEWLINE = 0x0A; // \n
+
+    public const _NULL_POINTER = 0x00;
+
+    public const _OPEN_BRACE = 0x7B; // {
+
+    public const _OPEN_BRACKET = 0x5B; // [
+
+    public const _OPEN_PARENTHESES = 0x28; // (
+
+    public const _PERCENT = 0x25; // %
+
+    public const _PLUS = 0x2B; // +
+
+    public const _QUESTION = 0x3F; // ?
+
+    public const _RETURN = 0x0D; // \r
+
+    public const _SEMICOLON = 0x3B; // ;
+
+    public const _SINGLE_QUOTE = 0x27; // '
+
+    public const _SLASH = 0x2F; // /
+
+    public const _SPACE = 0x20; // ' '
+
+    public const _TAB = 0x09; // \t
+
+    public const _TILDE = 0x7E; // ~
+
+    public const _UNDERSCORE = 0x5F; // _
+
+    public const _VERTICAL_TAB = 0x0B; // \v
+
+    public const a = 0x61;
+
+    public const A = 0x41;
+
+    public const b = 0x62;
+
+    public const B = 0x42;
+
+    public const c = 0x63;
+
+    public const C = 0x43;
+
+    public const d = 0x64;
+
+    public const D = 0x44;
+
+    public const e = 0x65;
+
+    public const E = 0x45;
+
+    public const f = 0x66;
+
+    public const F = 0x46;
+
+    public const g = 0x67;
+
+    public const G = 0x47;
+
+    public const h = 0x68;
+
+    public const H = 0x48;
+
+    public const i = 0x69;
+
+    public const I = 0x49;
+
+    public const j = 0x6A;
+
+    public const J = 0x4A;
+
+    public const k = 0x6B;
+
+    public const K = 0x4B;
+
+    public const l = 0x6C;
+
+    public const L = 0x4C;
+
+    public const m = 0x6D;
+
+    public const M = 0x4D;
+
+    public const n = 0x6E;
+
+    public const N = 0x4E;
+
+    public const o = 0x6F;
+
+    public const O = 0x4F;
+
+    public const p = 0x70;
+
+    public const P = 0x50;
+
+    public const q = 0x71;
+
+    public const Q = 0x51;
+
+    public const r = 0x72;
+
+    public const R = 0x52;
+
+    public const s = 0x73;
+
+    public const S = 0x53;
+
+    public const t = 0x74;
+
+    public const T = 0x54;
+
+    public const u = 0x75;
+
+    public const U = 0x55;
+
+    public const v = 0x76;
+
+    public const V = 0x56;
+
+    public const w = 0x77;
+
+    public const W = 0x57;
+
+    public const x = 0x78;
+
+    public const X = 0x58;
+
+    public const y = 0x79;
+
+    public const Y = 0x59;
+
+    public const z = 0x7A;
+
+    public const Z = 0x5a;
+}
