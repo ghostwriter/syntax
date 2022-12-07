eafd6004b743ff3fe37d8bb09f77fa1804267677
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-07 16:51:09 -0600

    Create SymbolKind.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Parser/SymbolKind.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Plex\Parser;
+
+final class SymbolKind
+{
+    public const Array = 18;
+
+    public const Boolean = 17;
+
+    public const Class = 5;
+
+    public const Constant = 14;
+
+    public const Constructor = 9;
+
+    public const Enum = 10;
+
+    public const EnumMember = 22;
+
+    public const Event = 24;
+
+    public const Field = 8;
+
+    public const File = 1;
+
+    public const Function = 12;
+
+    public const Interface = 11;
+
+    public const Key = 20;
+
+    public const Method = 6;
+
+    public const Module = 2;
+
+    public const Namespace = 3;
+
+    public const Null = 21;
+
+    public const Number = 16;
+
+    public const Object = 19;
+
+    public const Operator = 25;
+
+    public const Package = 4;
+
+    public const Property = 7;
+
+    public const String = 15;
+
+    public const Struct = 23;
+
+    public const TypeParameter = 26;
+
+    public const Variable = 13;
+}
