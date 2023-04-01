f3ac180936b183190311cd71486683d6692cfe77
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-01 11:47:44 -0500

    Update SymbolKind.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Parser/SymbolKind.php
+++ src/Parser/SymbolKind.php
@@ -2,59 +2,137 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Plex\Parser;
+namespace Ghostwriter\Syntax\Parser;
 
 final class SymbolKind
 {
+    /**
+     * @var int
+     */
     public const Array = 18;
 
+    /**
+     * @var int
+     */
     public const Boolean = 17;
 
+    /**
+     * @var int
+     */
     public const ClassSymbol = 5;
 
+    /**
+     * @var int
+     */
     public const Constant = 14;
 
+    /**
+     * @var int
+     */
     public const Constructor = 9;
 
+    /**
+     * @var int
+     */
     public const Enum = 10;
 
+    /**
+     * @var int
+     */
     public const EnumMember = 22;
 
+    /**
+     * @var int
+     */
     public const Event = 24;
 
+    /**
+     * @var int
+     */
     public const Field = 8;
 
+    /**
+     * @var int
+     */
     public const File = 1;
 
+    /**
+     * @var int
+     */
     public const Function = 12;
 
+    /**
+     * @var int
+     */
     public const Interface = 11;
 
+    /**
+     * @var int
+     */
     public const Key = 20;
 
+    /**
+     * @var int
+     */
     public const Method = 6;
 
+    /**
+     * @var int
+     */
     public const Module = 2;
 
+    /**
+     * @var int
+     */
     public const Namespace = 3;
 
+    /**
+     * @var int
+     */
     public const Null = 21;
 
+    /**
+     * @var int
+     */
     public const Number = 16;
 
+    /**
+     * @var int
+     */
     public const Object = 19;
 
+    /**
+     * @var int
+     */
     public const Operator = 25;
 
+    /**
+     * @var int
+     */
     public const Package = 4;
 
+    /**
+     * @var int
+     */
     public const Property = 7;
 
+    /**
+     * @var int
+     */
     public const String = 15;
 
+    /**
+     * @var int
+     */
     public const Struct = 23;
 
+    /**
+     * @var int
+     */
     public const TypeParameter = 26;
 
+    /**
+     * @var int
+     */
     public const Variable = 13;
 }
