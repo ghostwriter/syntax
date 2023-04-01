ccf8d0a01bc07cef43b7dc97e62e298d86794b10
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-01 11:47:24 -0500

    Update TokenTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Lexer/Token/TokenTest.php
+++ tests/Unit/Lexer/Token/TokenTest.php
@@ -4,23 +4,29 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Syntax\Tests\Unit\Lexer\Token;
 
+use Ghostwriter\Syntax\Tokenizer;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Small;
 use Ghostwriter\Syntax\Lexer\Token\Token;
 use PHPUnit\Framework\TestCase;
 
-/**
- * @coversDefaultClass \Ghostwriter\Syntax\Lexer\Token\Token
- *
- * @internal
- *
- * @small
- */
+#[Small]
+#[CoversClass(Token::class)]
 final class TokenTest extends TestCase
 {
+    /**
+     * @var string
+     */
+    private const TRIVIA = '  ';
+
+    /**
+     * @var string
+     */
+    private const BML = '#BlackLivesMatter';
+
     public function testToken(): void
     {
-        $trivia = '  ';
-        $bml = '#BlackLivesMatter';
-        $content = sprintf('%s%s', $trivia, $bml);
+        $content = sprintf('%s%s', self::TRIVIA, self::BML);
         $token = new Token(0, 0, 2, mb_strlen($content));
 
         self::assertSame(0, $token->getKind());
@@ -29,8 +35,8 @@ final class TokenTest extends TestCase
         self::assertSame(0, $token->getFullStart());
         self::assertSame(19, $token->getFullWidth());
 
-        self::assertSame($bml, $token->getText($content));
+        self::assertSame(self::BML, $token->getText($content));
         self::assertSame($content, $token->getFullText($content));
-        self::assertSame($trivia, $token->getTriviaText($content));
+        self::assertSame(self::TRIVIA, $token->getTriviaText($content));
     }
 }
