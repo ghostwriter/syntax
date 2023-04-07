307a3a893cde3b3d4ffccfa3aeb08bdd2a9a100e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-07 07:53:29 -0500

    Create KeywordKind.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/KeywordKind.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Syntax;
+
+final class KeywordKind
+{
+    public const T_ABSTRACT = 314;
+
+    public const T_AND_EQUAL = 277;
+
+    public const T_ARRAY_CAST = 298;
+
+    public const T_BOOL_CAST = 296;
+
+    public const T_BOOLEAN_AND = 284;
+
+    public const T_BOOLEAN_OR = 283;
+
+    public const T_CLONE = 305;
+
+    public const T_COALESCE = 282;
+
+    public const T_CONCAT_EQUAL = 275;
+
+    public const T_CONSTANT_ENCAPSED_STRING = 323;
+
+    public const T_DEC = 302;
+
+    public const T_DIV_EQUAL = 274;
+
+    public const T_DNUMBER = 318;
+
+    public const T_DO = 329;
+
+    public const T_DOUBLE_ARROW = 268;
+
+    public const T_DOUBLE_CAST = 300;
+
+    public const T_ECHO = 328;
+
+    public const T_ELSE = 308;
+
+    public const T_ELSEIF = 307;
+
+    public const T_ENCAPSED_AND_WHITESPACE = 322;
+
+    public const T_ENDIF = 309;
+
+    public const T_EVAL = 260;
+
+    public const T_EXIT = 326;
+
+    public const T_FINAL = 313;
+
+    public const T_IF = 327;
+
+    public const T_INC = 303;
+
+    public const T_INCLUDE = 262;
+
+    public const T_INCLUDE_ONCE = 261;
+
+    public const T_INLINE_HTML = 321;
+
+    public const T_INSTANCEOF = 294;
+
+    public const T_INT_CAST = 301;
+
+    public const T_IS_EQUAL = 285;
+
+    public const T_IS_GREATER_OR_EQUAL = 290;
+
+    public const T_IS_IDENTICAL = 287;
+
+    public const T_IS_NOT_EQUAL = 286;
+
+    public const T_IS_NOT_IDENTICAL = 288;
+
+    public const T_IS_SMALLER_OR_EQUAL = 291;
+
+    public const T_LNUMBER = 317;
+
+    public const T_LOGICAL_AND = 265;
+
+    public const T_LOGICAL_OR = 263;
+
+    public const T_LOGICAL_XOR = 264;
+
+    public const T_MINUS_EQUAL = 271;
+
+    public const T_MOD_EQUAL = 276;
+
+    public const T_MUL_EQUAL = 272;
+
+    public const T_NEW = 306;
+
+    public const T_NUM_STRING = 325;
+
+    public const T_OBJECT_CAST = 297;
+
+    public const T_OR_EQUAL = 278;
+
+    public const T_PLUS_EQUAL = 270;
+
+    public const T_POW = 304;
+
+    public const T_POW_EQUAL = 273;
+
+    public const T_PRINT = 266;
+
+    public const T_PRIVATE = 312;
+
+    public const T_PROTECTED = 311;
+
+    public const T_PUBLIC = 310;
+
+    public const T_REQUIRE = 259;
+
+    public const T_REQUIRE_ONCE = 258;
+
+    public const T_SL = 292;
+
+    public const T_SL_EQUAL = 280;
+
+    public const T_SPACESHIP = 289;
+
+    public const T_SR = 293;
+
+    public const T_SR_EQUAL = 281;
+
+    public const T_STATIC = 315;
+
+    public const T_STRING = 319;
+
+    public const T_STRING_CAST = 299;
+
+    public const T_STRING_VARNAME = 324;
+
+    public const T_UNSET_CAST = 295;
+
+    public const T_VARIABLE = 320;
+
+    public const T_WHILE = 330;
+
+    public const T_XOR_EQUAL = 279;
+
+    public const T_YIELD = 267;
+
+    public const T_YIELD_FROM = 269;
+    //    public const T_ENDWHILE
+}
+
