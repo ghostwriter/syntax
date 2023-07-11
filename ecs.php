3bef40eed090ace1db136d308f11b03ff2c5347f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-11 06:41:44 -0500

    Update ecs.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- ecs.php
+++ ecs.php
@@ -5,15 +5,18 @@ declare(strict_types=1);
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->import(__DIR__ . '/vendor/ghostwriter/coding-standard/ecs.php');
+    $ecsConfig->sets([
+        __DIR__ . '/vendor/ghostwriter/coding-standard/ecs.php',
+    ]);
+
     $ecsConfig->paths([
-        __DIR__ . '/bin',
         __DIR__ . '/ecs.php',
-        __DIR__ . '/README.md',
         __DIR__ . '/rector.php',
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
 
-    $ecsConfig->skip(['*/tests/Fixture/*', '*/vendor/*']);
+    $ecsConfig->skip([
+        __DIR__ . '/tests/Fixture/*', __DIR__ . '/vendor/*',
+    ]);
 };
